4dd0d1fd566d65cc77780dd4996a431a12103fae
kupriyanov_aa <kupriyanov_aa@teleformis.ru>
2023-01-11 22:17:38 +0300

    Initial commit. Configured liquibase.

---
+++ src/main/java/com/example/pizzaonline/SberTasteApplication.java
@@ -0,0 +1,13 @@
+package com.example.pizzaonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SberTasteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SberTasteApplication.class, args);
+    }
+
+}
